a7950c7bbc52301c3daf2b2409c2e52229c8cfe4
Tom Stejskal <tomstejskal@Toming-iMac.local>
2020-12-06 18:00:10 +0100

    Added main files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>stejsSource(JavaScript)</title>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/js/uikit.min.js"></script>
+    <script src="js/script.js"></script> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/css/uikit.min.css" />    
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body class="uk-padding">
+    
+    
+</body>
+</html>
